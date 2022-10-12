033a8f8f3451f85367716204dc28896e2caf9ed6
rachelcnc <115052360+rachelcnc@users.noreply.github.com>
2022-10-12 09:37:38 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,56 @@
-basic.forever(function () {
-    basic.showIcon(IconNames.Heart)
+let counter = 0
+input.onButtonPressed(Button.A, function () {
+    counter = 9
+    while (counter >= 0) {
+        basic.showNumber(counter)
+        counter += -1
+    }
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . # . # .
+        . . # . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . # . # .
+        # . . . #
+        . # . # .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . # . # .
+        # . . . #
+        . . . . .
+        # . . . #
+        . # . # .
+        `)
+    basic.showLeds(`
+        # . . . #
+        . . . . .
+        . . . . .
+        . . . . .
+        # . . . #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showString("2D03")
+})
+input.onButtonPressed(Button.B, function () {
+    counter = -1
 })
